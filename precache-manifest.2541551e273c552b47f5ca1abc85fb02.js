cb23add7f6cbf2377105c75c5fcd1862c23f38d4
jlgaleazzi <jgaleazzi@gmail.com>
2020-10-02 13:39:36 -0500

    deployed to github.io

---
+++ precache-manifest.2541551e273c552b47f5ca1abc85fb02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbd53da7960ef96932c149e9827a911",
+    "url": "/index.html"
+  },
+  {
+    "revision": "015ec0cc3084c4506f7c",
+    "url": "/static/css/main.00920afc.chunk.css"
+  },
+  {
+    "revision": "85bc153a5219be44a5cf",
+    "url": "/static/js/2.9b67901b.chunk.js"
+  },
+  {
+    "revision": "ef1c534d75b505369a52bc5e6f7e8d56",
+    "url": "/static/js/2.9b67901b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "015ec0cc3084c4506f7c",
+    "url": "/static/js/main.e74795d8.chunk.js"
+  },
+  {
+    "revision": "93c1be30ab044039f540",
+    "url": "/static/js/runtime-main.a9bca976.js"
+  }
+]);
